f36a4550ff857b082a67e33e2548ccff2dd3464c
cassiahahn <kklyra@gmail.com>
2022-02-05 12:58:44 +0100

    Second sharpen your pencil example

---
+++ chapter01/sharpen2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int c = 10;
+    while (c > 0)
+    {
+        puts ("I must not write code in class");
+        c = c - 1;
+    }
+    
+    return 0;
+}
